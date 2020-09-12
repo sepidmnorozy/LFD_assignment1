c077187e9205c780f71bb0655b55301d30033541
sepidmnoroozi <3pid.mollanoroozi@gmail.com>
2020-09-13 01:02:21 +0200

    everything in one file

--- LFDassignment1.py
+++ LFDassignment1.py
@@ -7,11 +7,13 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score as f_score
 from sklearn.metrics import recall_score as recall
 from sklearn.metrics import precision_score as precision
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, plot_confusion_matrix
+from sklearn.metrics import classification_report
 
 
 # COMMENT THIS
 # for reading the corpus and getting documents by using the strip and split methods
+# isolating the labels from the text
 # use_sentiment = 1 -> 2-class classification
 # use_sentiment = 0 -> 6-class classification
 def read_corpus(corpus_file, use_sentiment):
@@ -38,13 +40,22 @@ def identity(x):
 
 # COMMENT THIS
 # X = documents, Y = labels
-X, Y = read_corpus('trainset.txt', use_sentiment=False)
-# seprating the corpus into 75% for train and 25% for test
-split_point = int(0.75*len(X))
-Xtrain = X[:split_point]
-Ytrain = Y[:split_point]
-Xtest = X[split_point:]
-Ytest = Y[split_point:]
+# 2-class observation
+X_2, Y_2 = read_corpus('trainset.txt', use_sentiment=True)
+# separating the corpus into 75% for train and 25% for test
+split_point = int(0.75*len(X_2))
+Xtrain_2 = X_2[:split_point]
+Ytrain_2 = Y_2[:split_point]
+Xtest_2 = X_2[split_point:]
+Ytest_2 = Y_2[split_point:]
+
+# 6-class observation
+X_6, Y_6 = read_corpus('trainset.txt', use_sentiment=False)
+# separating the corpus into 75% for train and 25% for test
+Xtrain_6 = X_6[:split_point]
+Ytrain_6 = Y_6[:split_point]
+Xtest_6 = X_6[split_point:]
+Ytest_6 = Y_6[split_point:]
 
 # let's use the TF-IDF vectorizer
 tfidf = True
@@ -59,29 +70,48 @@ else:
                           tokenizer = identity)
 
 # combine the vectorizer with a Naive Bayes classifier
-classifier = Pipeline( [('vec', vec),
+classifier_2 = Pipeline( [('vec', vec),
+                        ('cls', MultinomialNB())] )
+classifier_6 = Pipeline( [('vec', vec),
                         ('cls', MultinomialNB())] )
 
 
 # COMMENT THIS
 # training the model
-classifier.fit(Xtrain, Ytrain)
+classifier_2.fit(Xtrain_2, Ytrain_2)
+classifier_6.fit(Xtrain_6, Ytrain_6)
+
 
 # COMMENT THIS
 # getting the predicted labels for test data from the model  
-Yguess = classifier.predict(Xtest)
+Yguess_2 = classifier_2.predict(Xtest_2)
+Yguess_6 = classifier_6.predict(Xtest_6)
 
 # COMMENT THIS
 # printing the accuracy_score result for measuring the model performance
-# print(accuracy_score(Ytest, Yguess))
+print("accuracy_score for 2-class")
+print(accuracy_score(Ytest_2, Yguess_2))
+print("accuracy_score for 6-class")
+print(accuracy_score(Ytest_6, Yguess_6))
 
-#fscore
-print(f_score(Ytest, Yguess))
-print(recall(Ytest, Yguess))
-print(precision(Ytest, Yguess))
+#1.3
+print("fscore, recall, precision for 2-class")
+print(classification_report(Ytest_2, Yguess_2))
+print("fscore, recall, precision for 6-class")
+print(classification_report(Ytest_6, Yguess_6))
 
+print("confusion_matrix for 2-class")
+print(confusion_matrix(Ytest_2, Yguess_2))
+print("confusion_matrix for 6-class")
+print(confusion_matrix(Ytest_6, Yguess_6))
 
-# 2-class accuracy_score : 0.782 
-# 6-class accuracy_score : 0.907
-
+import matplotlib.pyplot as plt
+plot_confusion_matrix(classifier_2, Xtest_2, Ytest_2)
+plot_confusion_matrix(classifier_6, Xtest_6, Ytest_6)
+plt.show()
+#1.4
+print("probabilities for 2_class")
+print(classifier_2.predict_proba(Xtest_2))
+print("probabilities for 6_class")
+print(classifier_6.predict_proba(Xtest_6))
 
